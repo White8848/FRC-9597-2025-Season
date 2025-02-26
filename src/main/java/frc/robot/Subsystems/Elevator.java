6e8fe3794224d9a7b00b39b75ed504eafa9e23ed
张苏杭 <534298627@qq.com>
2025-02-26 15:38:38 +0800

    change clawwheelintake method and container

--- src/main/java/frc/robot/Subsystems/Elevator.java
+++ src/main/java/frc/robot/Subsystems/Elevator.java
@@ -3,9 +3,7 @@ package frc.robot.Subsystems;
 import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
-import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.GravityTypeValue;
@@ -16,7 +14,6 @@ import static edu.wpi.first.units.Units.Volts;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase{ 
 
