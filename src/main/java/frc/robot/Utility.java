fac2ea01818ddbe746d31cf394431de618ff73b6
White8848 <2356758998@qq.com>
2025-03-02 18:48:38 -0500

    Add documentation for quadraticCurve methods in Utility class

--- src/main/java/frc/robot/Utility.java
+++ src/main/java/frc/robot/Utility.java
@@ -2,6 +2,12 @@ package frc.robot;
 
 public class Utility {
 
+    /**
+     * Quadratic curve function, with the range set to 1
+     * 
+     * @param x The input value
+     * @return The output value
+     */
     public static double quadraticCurve(double x) {
         if (x < 0) {
             return -quadraticCurve(-x);
@@ -9,6 +15,13 @@ public class Utility {
         return quadraticCurve(x, 1);
     }
 
+    /**
+     * Quadratic curve function
+     * 
+     * @param x     The input value
+     * @param range The range of the curve
+     * @return The output value
+     */
     public static double quadraticCurve(double x, double range) {
         if (x < 0) {
             return -quadraticCurve(-x, range);
